832d3684e83a62d6cb34ecdacff42519fcb295ca
Micah Baize <micah.baize@gmail.com>
2014-07-01 23:58:11 -0500

    Pset1-Mario
    The code for the Java version of CS50's Mario pset

---
+++ Pset1-Mario.java
@@ -0,0 +1,42 @@
+/**
+	User: mbaize
+	Date: 7/1/14
+	Time: 1:36am
+*/
+
+import java.io.Console;
+
+public class mario {
+
+	public static void main(String[] varArgs) {
+		
+		int rows;
+		do{
+			Console c = System.console();
+			System.out.print("How many rows high would you like the pyramid to be? (The number must be between 0 and 23, inclusive.) ");
+			rows = Integer.parseInt(System.console().readLine());
+			//rows = c.readLine("How many rows high would you like the pyramid to be? (The number must be between 0 and 23, inclusive.) ");
+		} while (rows < 0 || rows > 23);
+		
+		
+		for (int i = 0; i < rows; i++)
+		{
+			
+			for (int j = (rows - (i + 1)); j > 0; j--)
+			{
+				System.out.print(" ");
+			}
+			
+			for (int k = (i + 2); k > 0; k--)
+			{
+				System.out.print("#");
+			}
+			
+			System.out.print("\n");
+		
+		
+		}
+
+	}
+
+}
